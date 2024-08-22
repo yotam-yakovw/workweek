1230fd6fd20effccda25df304358ba9bb4ad69da
yotam-yakov <yotamyakov.w@gmail.com>
2024-08-23 00:55:08 +0300

    create basic structure and header

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,12 @@
+import './Header.css';
+
+export default function Header() {
+  return (
+    <header className="header">
+      <h1 href="/header" className="title">
+        Header
+      </h1>
+      <h2 className="icon">ICO</h2>
+    </header>
+  );
+}
