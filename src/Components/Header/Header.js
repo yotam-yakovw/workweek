e0ab4b62dc1d010b0aad06beb6ea3940daf6aaad
yotam-yakov <yotamyakov.w@gmail.com>
2024-08-25 20:55:42 +0300

    added date input in header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,17 @@
 import './Header.css';
 
 export default function Header() {
+  const onDateChange = (evt) => {
+    const day = new Date(evt.target.value);
+    console.log(day.getDay());
+  };
   return (
-    <header className="header">
-      <h1 href="/header" className="title">
+    <header className='header'>
+      <input type='date' onChange={onDateChange} className='date' />
+      <h1 href='/header' className='title'>
         Header
       </h1>
-      <h2 className="icon">ICO</h2>
+      <h3 className='icon'>ICO</h3>
     </header>
   );
 }
