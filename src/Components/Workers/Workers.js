bdd0f38aaba19a738e0e90068943fdfb1d24cd0b
yotam-yakov <yotamyakov.w@gmail.com>
2024-08-27 23:01:28 +0300

    reworked workers component based on data input

--- src/Components/Workers/Workers.js
+++ src/Components/Workers/Workers.js
@@ -1,14 +1,8 @@
 import './Workers.css';
 import AddButton from '../AddButton/AddButton';
-// import workersSlice from '../../redux/workersSlice';
-// import { useDispatch } from 'react-redux';
-import { workers as workersDB } from '../../temp/database';
+import dataEntry from '../../temp/database';
 
 export default function Workers() {
-  // const dispatch = useDispatch();
-
-  // const addWorker = () => dispatch(workersSlice.actions.addWorker());
-
   return (
     <>
       <div className='workers'>
@@ -18,33 +12,47 @@ export default function Workers() {
         <div className='day'>רביעי</div>
         <div className='day'>חמישי</div>
         <div className='day placeholder' />
-        {workersDB.map((worker, key) => (
+        {dataEntry.workers.map((worker, key) => (
           <Worker worker={worker} key={key} />
         ))}
       </div>
-      <AddButton onClick={() => console.log('clicked')} />
+      <AddButton onClick={() => console.log('click')} />
     </>
   );
 }
 
 function Worker({ worker }) {
+  const { id, name, days } = worker;
+  const onChange = (evt) => {
+    console.log(evt.target.id);
+  };
+
   return (
     <>
-      <textarea defaultValue={worker.name} className='worker-name' />
-      {worker.days.mor.map((text, key) => (
+      <textarea
+        defaultValue={name}
+        onChange={onChange}
+        className='worker-name'
+        id={id + '_name'}
+      />
+      {days.morning.map((text, key) => (
         <textarea
           defaultValue={text}
+          onChange={onChange}
           dir='rtl'
           key={key}
           className='textarea morning'
+          id={id + '_morning' + key}
         />
       ))}
-      {worker.days.ngt.map((text, key) => (
+      {days.night.map((text, key) => (
         <textarea
           defaultValue={text}
+          onChange={onChange}
           dir='rtl'
           key={key}
           className='textarea night'
+          id={id + '_night' + key}
         />
       ))}
     </>
