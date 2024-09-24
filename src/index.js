6dd146c97a60f2c05e0a488b0983e37342532143
yotam-yakov <yotamyakov.w@gmail.com>
2024-09-24 23:16:02 +0300

    turn app to pwa and corrected firebase initializa

--- src/index.js
+++ src/index.js
@@ -3,8 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './Components/App/App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyD_tABZetfOq0m24T3xpsUiwGm8vpSyFdA',
+  authDomain: 'yotam-yakov-project.firebaseapp.com',
+  projectId: 'yotam-yakov-project',
+  storageBucket: 'yotam-yakov-project.appspot.com',
+  messagingSenderId: '453996476647',
+  appId: '1:453996476647:web:2221e347ba8bc6a996ca42',
+  measurementId: 'G-5PMK678CKG',
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+console.log(analytics);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,6 +32,11 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
